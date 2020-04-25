ef1587940cef96ff650667c835a99fd0ddfa01e4
Christian Winter <christian.winter1990@gmail.com>
2020-04-26 00:41:54 +0200

    Add basic functionality

--- Backend/server.py
+++ Backend/server.py
@@ -13,22 +13,92 @@ PORT = 7777
 # Members
 open_connections = set()
 
-global_data = {
-    "counter": 0
-}
-
 async def handle_messages(websocket, path): # Will be called once per established connection
     await register(websocket)
     try:
         async for message in websocket:
             data = json.loads(message)
             print(data)
-            if data['action'] == 'increase':
-                global_data['counter'] += 1
-                await send(websocket, {'counter': global_data['counter']})
-            elif data['action'] == 'decrease':
-                global_data['counter'] -= 1
-                await send(websocket, {'counter': global_data['counter']})
+            if data['action'] == 'add_entry':
+                with open("data.json") as f:
+                    user_data = json.load(f)
+                try:
+                    current_data = user_data
+                except KeyError:
+                    user_data = {}
+                    current_data = user_data
+
+                for path_part in data['path']:
+                    current_data = current_data[path_part]
+
+                # Avoid overwriting
+                new_text = data['text']
+                or_new_text = new_text
+                counter = 2
+                while new_text in current_data:
+                    new_text = or_new_text + str(counter)
+                    counter += 1
+
+                # Add data
+                current_data[new_text] = {}
+
+                with open("data.json", 'w') as f:
+                    json.dump(user_data, f, indent=2)
+
+                await update_data(websocket)
+
+            elif data['action'] == 'delete_entry':
+                delete_path = data['path']
+                delete_element = delete_path[-1]
+                delete_path = delete_path[:-1]
+
+                print("will delete", delete_element, "from", delete_path)
+
+                with open("data.json") as f:
+                    user_data = json.load(f)
+                try:
+                    current_data = user_data
+                except KeyError:
+                    user_data = {}
+                    current_data = user_data
+
+                for path_part in delete_path:
+                    current_data = current_data[path_part]
+
+                del current_data[delete_element]
+
+
+                with open("data.json", 'w') as f:
+                    json.dump(user_data, f, indent=2)
+
+                await update_data(websocket)
+
+
+            elif data['action'] == 'rename_entry':
+                rename_path = data['path']
+                old_name = data['old']
+                new_name = data['new']
+
+                user_data, current_data = load_data()
+
+                for path_part in rename_path:
+                    current_data = current_data[path_part]
+
+                print("before")
+                print(json.dumps(current_data, indent=2))
+
+                current_data[new_name] = current_data[old_name]
+                del current_data[old_name]
+
+                print("after")
+                print(json.dumps(current_data, indent=2))
+
+                with open("data.json", 'w') as f:
+                    json.dump(user_data, f, indent=2)
+
+                await update_data(websocket)
+
+
 
 
     except asyncio.streams.IncompleteReadError:
@@ -38,6 +108,17 @@ async def handle_messages(websocket, path): # Will be called once per establishe
     finally:
         await unregister(websocket)
 
+def load_data():
+    with open("data.json") as f:
+        user_data = json.load(f)
+    try:
+        current_data = user_data
+    except KeyError:
+        user_data = {}
+        current_data = user_data
+
+    return user_data, current_data
+
 async def register(websocket):
     open_connections.add(websocket)
 
@@ -46,10 +127,14 @@ async def register(websocket):
         return uuid.uuid4().hex
 
     socket_id = _create_unique_socket_id()
+
+    # Send initial response
     await websocket.send(json.dumps({
         'type': 'connection_established',
-        'id': socket_id,
+        'id': socket_id
     }))
+
+    await update_data(websocket)
 
     # Send current game state to new user
     # await websocket.send(gameserver.get_serialized_state())
@@ -63,6 +148,17 @@ async def send(websocket, data):
 async def unregister(websocket):
     open_connections.remove(websocket)
 
+
+async def update_data(websocket):
+    # Load user data
+    with open("data.json") as f:
+        user_data = json.load(f)
+
+    await websocket.send(json.dumps({
+        'type': 'update_data',
+        'data': user_data
+    }))
+
 # Start server loop
 print("Starting server on port " + str(PORT))
 asyncio.get_event_loop().run_until_complete(websockets.serve(handle_messages, ADDRESS, PORT))
@@ -70,3 +166,4 @@ asyncio.get_event_loop().run_until_complete(websockets.serve(handle_messages, AD
 path = os.getcwd()
 print(f"Server running on {path}/Frontend/index.html")
 asyncio.get_event_loop().run_forever()
+
